5e4a12c375dcd12660c15c156168064796db192e
ramzannoon <ramzannoon675@gmail.com>
2023-01-14 11:09:12 +0000

    update vendor ad

--- nn_vendor_management/__manifest__.py
+++ nn_vendor_management/__manifest__.py
@@ -24,8 +24,9 @@
 
     # always loaded
     'data': [
-        'security/ir.model.access.csv',
         'security/security.xml',
+        'security/ir.model.access.csv',
+        
         'views/nn_res_partner_views.xml',
     ],
     # only loaded in demonstration mode
--- nn_vendor_management/models/.ipynb_checkpoints/nn_res_partner-checkpoint.py
+++ nn_vendor_management/models/.ipynb_checkpoints/nn_res_partner-checkpoint.py
@@ -7,16 +7,16 @@ class NNResPartner(models.Model):
     _name = 'nn.res.partner'
     _description = 'Vendor Management'
 
-    name = fields.Char(string='Name')
+    name = fields.Char(string='Name', required=True)
     company_id = fields.Many2one('res.company', string='Company', efault=lambda self: self.env.user)
     vendor_company = fields.Char(string='Vendor Company', required=True)
-    poc = fields.Char(string='POint Of Contract' , required=True)
+    poc = fields.Char(string='Point Of Contract' , required=True)
     email = fields.Char(string='Email')
     phone = fields.Char(string='Phone')
     region = fields.Char(string='Region')
     country_id = fields.Many2one('res.country', string='Country')
-    city_id = fields.Many2one('res.city', string='City')
-    rate = fields.Char(string='Rate')
+    city_id = fields.Char(string='City')
+    rate = fields.Char(string='Rate', required=True)
     remarks = fields.Char(string='Remarks')
     
     
